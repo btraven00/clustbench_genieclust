ac1cc2bdcd70b44a6627d2cc7b9487fd2d5a0e9d
Izaskun Mallona <izaskun.mallona@gmail.com>
2025-01-16 09:28:10 +0100

    Add genie, ica, gic

--- run_genieclust.py
+++ run_genieclust.py
@@ -33,21 +33,58 @@ def load_dataset(data_file):
     return(data)
 
 
-def do_genie(X, ks):
+def do_genie(X, Ks, g):
     res = dict()
     
     genie = genieclust.Genie(
         postprocess="all"
     )
-    # how to define the gini thres?
-    sys.exit()
-    for g in [0.1, 0.3, 0.5, 0.7, 1.0]:
-        for K in Ks:
-            genie.set_params(gini_threshold=g)
-            genie.set_params(n_clusters=K)
-            labels_pred = genie.fit_predict(X)+1 # 0-based -> 1-based!!!
+
+    for K in Ks:
+        genie.set_params(gini_threshold=g)
+        genie.set_params(n_clusters=K)
+        labels_pred = genie.fit_predict(X)+1 # 0-based -> 1-based!!!
+        res[K] = labels_pred
+        print(res[K])
+    
+    return np.array([res[key] for key in res.keys()]).T
+
+def do_gic(X, Ks):
+    res = dict()
+    
+    # do not use compute_all_cuts! - see note on add_clusters in the manual
+    gic = genieclust.GIc(
+        compute_full_tree=False,
+        compute_all_cuts=False,
+        postprocess="all")
+
+    for K in Ks:
+        gic.set_params(n_clusters=K)
+        labels_pred = gic.fit_predict(X)+1 # 0-based -> 1-based!!!
+        if gic.n_clusters_ == K:
+            # due to noise points, some K-partitions might be unavailable
             res[K] = labels_pred
-    return res
+    
+    return np.array([res[key] for key in res.keys()]).T
+
+def do_ica(X, Ks):
+    res = dict()
+
+    ica = genieclust.GIc(
+        n_clusters=max(Ks),
+        compute_full_tree=True,
+        postprocess="all",
+        compute_all_cuts=True,
+        gini_thresholds=[] # this is IcA -- start from n singletons
+    )
+
+    labels_pred_matrix = ica.fit_predict(X)+1 
+    #print(labels_pred_matrix)
+    for K in Ks:
+        res[K] = labels_pred_matrix[K]        
+        #print(res[K])
+    
+    return np.array([res[key] for key in res.keys()]).T
 
 def main():
     parser = argparse.ArgumentParser(description='clustbench geniecluster runner')
@@ -62,7 +99,9 @@ def main():
     parser.add_argument('--method', type=str,
                         help='geniecluster method',
                         required = True)
-
+    parser.add_argument('--gini_threshold', type=str,
+                        help='g',
+                        required = False)
     try:
         args = parser.parse_args()
     except:
@@ -71,19 +110,24 @@ def main():
 
     if args.method not in VALID_METHODS:
         raise ValueError(f"Invalid method `{args.method}`")
-
+    
     truth = load_labels(getattr(args, 'data.true_labels'))
     k = int(max(truth)) # true number of clusters
     Ks = [k-2, k-1, k, k+1, k+2] # ks tested, including the true number
     
     data = getattr(args, 'data.matrix')
-    curr = do_benchmark_fastcluster_range_ks(X= load_dataset(data), Ks = Ks, method = args.method)
+
+    if args.method == 'genie':        
+        curr = do_genie(X= load_dataset(data), Ks = Ks, g = float(args.gini_threshold))
+    elif args.method == 'gic':
+        curr = do_gic(X= load_dataset(data), Ks = Ks)
+    elif args.method == 'ica':
+        curr = do_ica(X= load_dataset(data), Ks = Ks)
 
     name = args.name
 
     header=['k=%s'%s for s in Ks]
-
-
+    
     curr = np.append(np.array(header).reshape(1,5), curr.astype(str), axis=0)
     np.savetxt(os.path.join(args.output_dir, f"{name}_ks_range.labels.gz"),
                curr, fmt='%s', delimiter=",")#,
